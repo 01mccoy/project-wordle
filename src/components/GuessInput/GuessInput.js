fca8edea0ff17e47b9216d2386c575402ebc3796
01mccoy <unogle12@gmail.com>
2023-10-26 18:05:56 +0900

    feat: GuessInput 컴포넌트 생성 후 Game 에 추가

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { WORD_LENGTH } from "../../constants";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.info({ guess });
+    setGuess("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(event) => {
+          setGuess(event.target.value.toUpperCase());
+        }}
+        pattern="\w{5,5}"
+        title="5개의 글자를 영어로 입력해주세요."
+        minLength={WORD_LENGTH}
+        maxLength={WORD_LENGTH}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
