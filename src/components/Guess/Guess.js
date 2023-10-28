6341028bd9f4ddbc81bc3ef89958d0b9cbf32dfa
01mccoy <unogle12@gmail.com>
2023-10-28 21:14:06 +0900

    feat: guess 와 answer 비교 결과 보여주기

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,14 +2,25 @@ import React from "react";
 
 import { WORD_LENGTH } from "../../constants";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
+
+import { answer } from "../Game";
+
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : "cell";
+  return <span className={className}>{letter}</span>;
+}
 
 function Guess({ guess }) {
+  const result = guess ? checkGuess(guess, answer) : undefined;
   return (
     <p className="guess">
       {range(WORD_LENGTH).map((index) => (
-        <span key={index} className="cell">
-          {guess ? guess[index] : ""}
-        </span>
+        <Cell
+          key={index}
+          letter={result ? result[index].letter : undefined}
+          status={result ? result[index].status : undefined}
+        />
       ))}
     </p>
   );
